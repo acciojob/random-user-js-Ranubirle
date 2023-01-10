d39a7c22081b1c7553f6bde5100f26ef949eb1c2
Ranubirle <115343697+Ranubirle@users.noreply.github.com>
2023-01-10 18:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
-
+const p=fetch("https://randomuser.me/api/");
+p.then(data=>{
+ console.log(data);
+	return data.json();
+})
+.then(data=>{
+	console.log(data);
+})
+const ans=data;
