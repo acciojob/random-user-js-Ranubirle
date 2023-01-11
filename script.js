1b9f8c847342a1d4c12715c4155927a214a55088
Ranubirle <115343697+Ranubirle@users.noreply.github.com>
2023-01-11 11:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 let p=fetch("https://randomuser.me/api/");
 p.then(data=>{
- console.log(data);
+ 
 	return data.json();
 })
 .then(data=>{
 	console.log(data);
 	let info=data;
-	document.getElementById('age').innertext=info.results[0].dob[0].age;
+	
 	
 
 	
